ff11848b35d475f9f61d76ea4ed1bad85df137ad
Berdin Matondo <berdinmatondo7@gmail.com>
2025-02-11 07:09:28 +0100

    tool edit

--- start/kernel.ts
+++ start/kernel.ts
@@ -39,5 +39,6 @@ router.use([() => import('@adonisjs/core/bodyparser_middleware'), () => import('
  * the routes or the routes group.
  */
 export const middleware = router.named({
-  auth: () => import('#middleware/auth_middleware')
+  isAdmin: () => import('#middleware/is_admin_middleware'),
+  auth: () => import('#middleware/auth_middleware'),
 })
--- start/routes.ts
+++ start/routes.ts
@@ -7,10 +7,21 @@
 |
 */
 
+import UsersController from '#controllers/users_controller'
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
+})
+router.group(() => {
+  // Création de comptes
+  router.post('/register/client', [UsersController, 'createClient'])
+  router.post('/register', [UsersController, 'store'])
+  router.post('/register/admin', [UsersController, 'createAdmin'])
+  // Connexion & Déconnexion
+  router.post('/login', [UsersController, 'login'])
+  router.post('/logout', [UsersController, 'logout']).use(middleware.auth({ guards: ['api'] }))
 })
