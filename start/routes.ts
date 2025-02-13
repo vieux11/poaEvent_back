7f7de5e969bfd8a25e573b9c0f148abc255bbbe3
Berdin Matondo <berdinmatondo7@gmail.com>
2025-02-13 13:27:03 +0100

    routes: event, reservation

--- start/routes.ts
+++ start/routes.ts
@@ -10,6 +10,8 @@
 import UsersController from '#controllers/users_controller'
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
+import EventsController from '#controllers/events_controller'
+import ReservationsController from '#controllers/reservations_controller'
 
 router.get('/', async () => {
   return {
@@ -24,4 +26,26 @@ router.group(() => {
   // Connexion & Déconnexion
   router.post('/login', [UsersController, 'login'])
   router.post('/logout', [UsersController, 'logout']).use(middleware.auth({ guards: ['api'] }))
+})
+//event route groupe
+
+router.get('/events', [EventsController, 'index'])
+// Création des events
+router
+  .post('/createEvent', [EventsController, 'store'])
+  .use([middleware.auth({ guards: ['api'] }), middleware.isAdmin()])
+// récupération des events d'un admin
+router
+  .get('/getMyEvents', [EventsController, 'getMyEvents'])
+  .use([middleware.auth({ guards: ['api'] }), middleware.isAdmin()])
+router.group(() => {
+  // Création d'une réservation (client uniquement)
+  router.post('/createReservation', [ReservationsController, 'store'])
+  // .use([middleware.auth({ guards: ['api'] })])
+  // Récupérer les réservations du client authentifié
+  router.get('/getMyReservations', [ReservationsController, 'getMyReservations'])
+    .use([middleware.auth({ guards: ['api'] })])
+  // Récupérer toutes les réservations (admin uniquement)
+  router.get('/getAllReservations', [ReservationsController, 'getAllReservations'])
+    .use([middleware.auth({ guards: ['api'] }), middleware.isAdmin()])
 })
